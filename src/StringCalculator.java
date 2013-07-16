92c10f6dd0246213bf08900ca7d3af1d42e330d7
Remko de Jong <aggenebbisj@gmail.com>
2013-07-16 23:10:17 +0200

    success

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -5,69 +5,103 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-	private static final String REGEX_SINGLE_CHAR_DELIMITER = "^//(.)\n";
-	private static final String REGEX_MULTI_CHAR_DELIMITER = "^//\\[(.+?)\\]\n";
-	
-	private static final String REGEX_SINGLE = REGEX_SINGLE_CHAR_DELIMITER + "(.*)";
-	private static final String REGEX_MULTI = REGEX_MULTI_CHAR_DELIMITER + "(.*)";
-	
+	private static final String REGEX_INPUT = "//((.)|(\\[(.+?)\\]))\n(.*)";
+	private static final String REGEX_BRACKETED_DELIMITERS = "\\[(.+?)\\]";
+	private static final int REGEX_GROUP_SINGLE_CHAR_DELIMITER = 1;
+	private static final int REGEX_GROUP_BRACKETED_DELIMITERS = 3;
+	private static final int REGEX_GROUP_NUMBER_STRING = 5;
+
+	private static final String[] DEFAULT_DELIMITERS = new String[] {",", "\n"};
+	private static final int NUMBER_UPPER_BOUND = 1000;
+
 	private List<Integer> errors = new ArrayList<>();
 
 	public int add(String input) {
+		if (input.isEmpty())
+			return 0;
+
 		Matcher m = Pattern.compile(REGEX_INPUT).matcher(input);
-		return sum(splitNumberString(getNumberString(input, m), getDelimiters(m)));
+		return sum(getNumbers(getNumberString(input, m), getDelimiters(m)));
 	}
 
 	private String getNumberString(String numberString, Matcher m) {
 		if (m.reset().find()) {
-			return m.group(2);
+			numberString = m.group(REGEX_GROUP_NUMBER_STRING);
 		}
 		return numberString;
 	}
 
-	private String getDelimiters(Matcher m) {
+	private String[] getDelimiters(Matcher m) {
 		if (m.reset().find()) {
-			String result = m.group(1);
-			System.out.println(result);
-			return Pattern.quote(result);
+			return getUserDefinedDelimiter(m);
 		}
-		return ",|\n";
+		return DEFAULT_DELIMITERS;
+	}
+
+	private String[] getUserDefinedDelimiter(Matcher m) {	
+		return hasBracketedDelimiter(m) ? getBracketedDelimiters(m) : getSingleCharDelimiter(m);
+	}
+	
+	private String[] getSingleCharDelimiter(Matcher m) {
+		return new String[] { m.group(REGEX_GROUP_SINGLE_CHAR_DELIMITER) };
+	}
+
+	private boolean hasBracketedDelimiter(Matcher m) {
+		return m.group(REGEX_GROUP_BRACKETED_DELIMITERS) != null;
+	}
+	
+	private String[] getBracketedDelimiters(Matcher m) {
+		String bracketedDelimiters = m.group(REGEX_GROUP_BRACKETED_DELIMITERS);
+		Matcher d = Pattern.compile(REGEX_BRACKETED_DELIMITERS).matcher(bracketedDelimiters);
+		
+		List<String> result = new ArrayList<>();
+		while (d.find()) {
+			result.add(d.group(1));
+		}
+		return result.toArray(new String[result.size()]);
+	}
+
+	private String[] getNumbers(String input, String[] delimiters) {
+		return input.split(implode(delimiters));
+	}
+
+	private String implode(String[] delimiters) {
+		StringBuilder result = new StringBuilder();
+		for (String delim : delimiters) {
+			if (result.length() == 0) {
+				result.append(Pattern.quote(delim)); 
+			} else {
+				result.append("|" + Pattern.quote(delim)); 
+			}
+		}
+		return result.toString();
 	}
 
 	private int sum(String[] numbers) {
 		int sum = 0;
-		for (String numberString : numbers) {
-			sum += toInt(numberString);
+		for (String number : numbers) {
+			sum += toInt(number);
 		}
 
-		if (!errors.isEmpty())
-			throw new IllegalArgumentException("negatives not allowed " + errors.toString());
-		
+		validateForNegativeNumbers();
 		return sum;
 	}
 
-	private String[] splitNumberString(String numbers, String delimiters) {
-		if (numbers.isEmpty())
-			return new String[0];
-
-		return numbers.split(delimiters);
+	private void validateForNegativeNumbers() {
+		if (!errors.isEmpty())
+			throw new IllegalArgumentException("negatives not allowed " + errors.toString());
 	}
 
-	private int toInt(String number) {
-		int result = Integer.parseInt(number);
-		validateForNegatives(result);
-		return ignoreValuesGreaterThan1000(result);
-	}
+	private int toInt(String input) {
+		int value = Integer.parseInt(input);
 
-	private int ignoreValuesGreaterThan1000(int result) {
-		if (result > 1000)
-			result = 0;
-		return result;
-	}
+		if (value < 0)
+			errors.add(value);
 
-	private void validateForNegatives(int result) {
-		if (result < 0)
-			errors.add(result);
+		if (value > NUMBER_UPPER_BOUND)
+			return 0;
+		
+		return value;
 	}
 
 }
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -3,9 +3,12 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import org.junit.Assert;
-import org.junit.Test;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
 
 public class StringCalculatorTest {
 
@@ -20,37 +23,37 @@ public class StringCalculatorTest {
 	public void should_return_the_number_for_string_with_single_number() {
 		assertThat(calculator.add("1"), is(1));
 	}
-	
+
 	@Test
 	public void should_return_sum_for_two_numbers() {
 		assertThat(calculator.add("1,2"), is(3));
 	}
-	
+
 	@Test
 	public void should_return_sum_for_any_amount_of_numbers() {
 		assertThat(calculator.add("1,2,3,4"), is(10));
 	}
-	
+
 	@Test
 	public void should_handle_newline_as_delimiter() {
 		assertThat(calculator.add("1,2\n3"), is(6));
 	}
-	
+
 	@Test
 	public void should_support_custom_delimiter() {
 		assertThat(calculator.add("//;\n1;2;3"), is(6));
 	}
-	
+
 	@Test
 	public void should_support_reserved_character_as_custom_delimiter() {
 		assertThat(calculator.add("//$\n1$2$3"), is(6));
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void should_throw_exception_for_negative_number() {
 		calculator.add("-1");
 	}
-	
+
 	@Test
 	public void should_list_negative_number_in_exception() {
 		try {
@@ -60,7 +63,7 @@ public class StringCalculatorTest {
 			assertTrue(e.getMessage().contains("-1"));
 		}
 	}
-	
+
 	@Test
 	public void should_list_all_negative_numbers_in_exception() {
 		try {
@@ -71,19 +74,25 @@ public class StringCalculatorTest {
 			assertTrue(e.getMessage().contains("-3"));
 		}
 	}
-	
+
 	@Test
 	public void should_ignore_numbers_larger_than_1000() {
 		assertThat(calculator.add("2,1001"), is(2));
 	}
-	
+
 	@Test
-	public void should_support_delimiters_of_any_length() {
+	public void should_support_multi_character_delimiters() {
 		assertThat(calculator.add("//[***]\n1***2***3"), is(6));
 	}
-	
+
 	@Test
-	public void should_support_multiple_delimiters() {
+	public void should_support_multiple_single_character_delimiters() {
 		assertThat(calculator.add("//[*][%]\n1*2%3"), is(6));
 	}
+	
+	@Test
+	public void should_support_multiple_multi_character_delimiters() {
+		assertThat(calculator.add("//[***][%%%]\n1***2%%%3"), is(6));
+	}
+	
 }
